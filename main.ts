63330c0dc3b432bb3860332e3076774b1846831c
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-02-12 23:04:02 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    RainbowSparkleUnicorn.Light.turnOn(lightPins.P8)
+    RainbowSparkleUnicorn.Light.breathe(
+    breathLightPins.P15,
+    1000,
+    1000,
+    500,
+    500
+    )
 })
 input.onButtonPressed(Button.B, function () {
-    RainbowSparkleUnicorn.Light.turnOff(lightPins.P8)
+    RainbowSparkleUnicorn.Light.turnOff(lightPins.P15)
 })
 comment.comment("To use the board you must add this block below as the first block")
 RainbowSparkleUnicorn.start()
